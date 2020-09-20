b7650d10d7a63a0a41d56791bc6021f20059ebfa
Jerry777777 <491818175@qq.com>
2020-09-20 22:37:24 +0800

    [xiawenhao] faet: add if the purchase fails, bad request is returned

--- src/main/java/com/thoughtworks/rslist/service/RsService.java
+++ src/main/java/com/thoughtworks/rslist/service/RsService.java
@@ -6,11 +6,14 @@ import com.thoughtworks.rslist.dto.RsEventDto;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.dto.VoteDto;
 import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.TradeRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,11 +21,14 @@ public class RsService {
   final RsEventRepository rsEventRepository;
   final UserRepository userRepository;
   final VoteRepository voteRepository;
+  final TradeRepository tradeRepository;
+  final List<Integer> priceOfRanking = new ArrayList<>();
 
-  public RsService(RsEventRepository rsEventRepository, UserRepository userRepository, VoteRepository voteRepository) {
+  public RsService(RsEventRepository rsEventRepository, UserRepository userRepository, VoteRepository voteRepository, TradeRepository tradeRepository) {
     this.rsEventRepository = rsEventRepository;
     this.userRepository = userRepository;
     this.voteRepository = voteRepository;
+    this.tradeRepository = tradeRepository;
   }
 
   public void vote(Vote vote, int rsEventId) {
@@ -49,7 +55,14 @@ public class RsService {
     rsEventRepository.save(rsEvent);
   }
 
-  public void buy(Trade trade, int id) {
-
+  public boolean buy(Trade trade, int id) {
+    int wantedRank = trade.getRank();
+    int purchaseAmount = trade.getAmount();
+    int rsEventId = trade.getRsEventId();
+    Optional<RsEventDto> foundRsEvent = rsEventRepository.findById(rsEventId);
+    if (!foundRsEvent.isPresent()) {
+      throw new RuntimeException();
+    }
+    return priceOfRanking.size() < wantedRank || priceOfRanking.get(wantedRank - 1) < purchaseAmount;
   }
 }
